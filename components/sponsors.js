142a8fafd641924feeff19c1def7dd6af5bc4572
jamestq <57053746+jamestq@users.noreply.github.com>
2023-02-22 20:48:51 +1100

    add sponsor banner

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,18 +1,34 @@
 import styles from '../styles/Sponsors.module.css'
+import Image from 'next/image';
+
+import eyLogo from '../assets/ey.png';
+import qrLogo from '../assets/quickrelease.png';
 
 const sponsors = [
     {
         title: "EY",
-        image: "",
+        image: eyLogo,
     },
     {
         title: "Quick Release",
-        image: "",
+        image: qrLogo,
     }
 ]
 
 export default function Sponsors(){
     return <div className={styles.container}>
         <h1>Our Sponsors</h1>
+        <div className={styles.sponsorContainer}>
+            {
+                sponsors.map(sponsor => {
+                    return <div className={styles.logoContainer}>
+                            <Image 
+                            key={sponsor.title}
+                            src={sponsor.image}
+                            />
+                    </div>
+                })
+            }
+        </div>
     </div>
 }
